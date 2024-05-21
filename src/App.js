5e5e6b2e1866b66c918ad6d62bcde650aca092f6
Mohit Choubey <mohitjdh2021@gmail.com>
2024-05-21 20:07:39 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {Home} from './pages/home.js';
+import {Auth} from './pages/auth.js';
+import {SavedRecipes} from './pages/saved-recipe.js';
+import {CreateRecipe} from './pages/create-recipe.js';
 import './App.css';
-
+import {Navbar} from "./componenets/navbar.js"
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/auth" element={<Auth></Auth>}/>
+        <Route path="/saved-receipe" element={<SavedRecipes></SavedRecipes>}/>
+        <Route path="/create-receipe" element={<CreateRecipe></CreateRecipe>}/>
+      </Routes>
+    </Router>
     </div>
   );
 }
